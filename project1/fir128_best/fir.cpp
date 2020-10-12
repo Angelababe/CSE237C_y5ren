a50d695673a678fd77a7d4e4e95e5c71caeda65d
Yiming REN <57914412+Angelababe@users.noreply.github.com>
2020-10-12 02:44:35 -0700

    Add files via upload

---
+++ project1/fir128_best/fir.cpp
@@ -0,0 +1,38 @@
+/*
+	Filename: fir.cpp
+		FIR lab wirtten for WES/CSE237C class at UCSD.
+		Match filter
+	INPUT:
+		x: signal (chirp)
+
+	OUTPUT:
+		y: filtered output
+
+*/
+
+#include "fir.h"
+
+void fir (
+  data_t *y,
+  data_t x
+  )
+{
+
+	coef_t c[N] = {10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+	static char data[128];
+#pragma HLS array_partition variable=data cyclic factor=2
+		acc_t acc=0;
+		for(int i=127; i>0; i--){
+#pragma HLS unroll factor=16 skip_exit_check
+#pragma HLS PIPELINE II=1
+			//shift
+			data[i]=data[i-1];
+			//accumulate&&multiply
+			acc+=data[i]*c[i];
+		}
+		data[0] = x;
+		acc+=data[0]*c[0];
+		*y=acc;
+}
+
+
